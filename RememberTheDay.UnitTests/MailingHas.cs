05f456b4b4316c5a3e03187b6271f59f73a10930
Sergey Lobin <ifitwasi@gmail.com>
2018-02-24 13:42:49 +0300

    adding Domain & Unit Tests

---
+++ RememberTheDay.UnitTests/MailingHas.cs
@@ -0,0 +1,89 @@
+using System;
+using NUnit.Framework;
+
+namespace RememberTheDay.UnitTests
+{
+    [TestFixture]
+    public class MailingHas
+    {
+
+        [Test]
+        public void NoPersons_WhenNoPersonsAdded()
+        {
+            // Arrange
+            Mailing m = new Mailing();
+            
+                        
+            // Assert
+            Assert.AreEqual(m.MailingList.Count, 0);
+        }
+        
+        
+        [Test]
+        public void OnePerson_WhenAddedOnlyOnePerson()
+        {
+            // Arrange
+            Mailing m = new Mailing();
+            
+            // Act
+            m.addRecipient(new Person("a", "b"));
+            
+            
+            // Assert
+            Assert.AreEqual(m.MailingList.Count, 1);
+        }
+        
+        
+        [Test]
+        public void OnePerson_AndHeHasBirthDay_ThenListIsEmpty()
+        {
+            // Arrange
+            Mailing m = new Mailing();
+            Person p = new Person("a", "b");
+            m.addRecipient(p);
+            
+            
+            // Act
+            m.getRecipients(p);
+            
+            
+            // Assert
+            Assert.AreEqual(m.MailingList.Count, 0);
+        }  
+        
+        [Test]
+        public void AddedOnePerson_AndAnotherGuyHasBirthDay_ThenListHasOnePerson()
+        {
+            // Arrange
+            Mailing m = new Mailing();
+            Person p = new Person("a", "b");
+            m.addRecipient(p);
+            
+            
+            // Act
+            m.getRecipients(new Person("c", "d"));
+            
+            
+            // Assert
+            Assert.AreEqual(m.MailingList.Count, 1);
+        } 
+        
+        
+        [Test]
+        public void AddedTwoPersons_AndOneOfThemHasBirthDay_ThenListHasAnotherPerson()
+        {
+            // Arrange
+            Mailing m = new Mailing();
+            Person p = new Person("a", "a");
+            m.addRecipient(p);
+            p = new Person("b", "b");
+            m.addRecipient(p);
+            
+            // Act
+            m.getRecipients(new Person("a", "a"));
+                        
+            // Assert
+            Assert.AreEqual(m.MailingList.ToArray()[0].Name, "b");
+        }        
+    }
+}
---
+++ RememberTheDay/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RememberTheDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mailing ml = new Mailing();
+
+            
+            // adding two persons to the mailing list
+            ml.addRecipient(new Person("Mary Jane", "mary.jane@myorg.com"));
+            ml.addRecipient(new Person("Kate Stark", "kate.stark@myorg.com"));
+
+            // and John
+            Person john = new Person("John Doe", "john.doe@myorg.com");
+            ml.addRecipient(john);
+            
+            // who has a Birth Day today 
+            List<Person> recipients = ml.getRecipients(john);
+
+            // get only emails
+            List<string> emails = new List<string>();
+            foreach (var r in recipients)
+            {
+                emails.Add(r.Email);
+            }
+
+            // Let's see what we have
+            Console.WriteLine("Mail list:");
+            Console.WriteLine(String.Join(", ", emails));
+        }
+    }
+
+    public class Person
+    {
+        public string Name;
+        public string Email;
+
+        public Person(string _Name, string _Email)
+        {
+            Name = _Name;
+            Email = _Email;
+        }
+    }
+    
+    public class Mailing
+    {
+        public List<Person> MailingList = new List<Person>();
+
+        public void addRecipient(Person person)
+        {
+            MailingList.Add(person);
+        }
+
+        public List<Person> getRecipients(Person celebrant)
+        {
+            var i = MailingList.FindIndex(x => celebrant.Name == x.Name);
+            
+            if (i >= 0)
+            {
+                MailingList.RemoveAt(i);    
+            }
+            
+            return MailingList;
+        }
+        
+    }
+}
