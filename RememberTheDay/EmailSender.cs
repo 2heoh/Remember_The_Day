45b71f873a609bdfb67c3d67504792df7d83a60b
Sergey Lobin <ifitwasi@gmail.com>
2018-02-28 10:51:47 +0300

    now we can send emails

---
+++ RememberTheDay/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Mail;
+using System.Reflection;
+
+namespace RememberTheDay
+{
+    public class EmailSender
+    {
+        
+        private SmtpClient _client;
+        private string _host = "smtp.gmail.com";
+        private int _port = 25;
+        private string emailFrom = "you@yourcompany.com";
+
+        public EmailSender()
+        {
+            _client = new SmtpClient();
+            _client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            _client.UseDefaultCredentials = false;
+            _client.Host = _host;    
+            _client.Port = _port;
+            
+        }
+        
+        public EmailSender(string host, int port)
+        {
+            _client = new SmtpClient();
+            _client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            _client.UseDefaultCredentials = false;
+            
+            _client.Host = host;    
+            _client.Port = port;
+            
+        }
+
+        public void Send(string emailTo , string subject, string body)
+        {
+            MailMessage mail = new MailMessage(emailFrom, emailTo);
+
+            mail.Subject = subject;
+            mail.Body = body;
+            _client.Send(mail);
+
+        }
+        
+    }
+}
--- RememberTheDay/Program.cs
+++ RememberTheDay/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 
 namespace RememberTheDay
 {
@@ -66,6 +66,14 @@ namespace RememberTheDay
             }
             
             return MailingList;
+        }
+
+        public void Send (string title, string message)
+        {
+
+            EmailSender sender = new EmailSender(); 
+            
+            MailingList.ForEach( x => sender.Send(x.Email, title, message));
         }
         
     }
